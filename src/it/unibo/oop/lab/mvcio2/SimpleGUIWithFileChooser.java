7ff20c904269d874b1b15d897805f11c70d83a4b
alex presepi <alex.presepi@studio.unibo.it>
2021-11-17 21:53:19 +0100

    Completed GUI mvcio2

--- src/it/unibo/oop/lab/mvcio2/SimpleGUIWithFileChooser.java
+++ src/it/unibo/oop/lab/mvcio2/SimpleGUIWithFileChooser.java
@@ -1,5 +1,24 @@
 package it.unibo.oop.lab.mvcio2;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import it.unibo.oop.lab.mvcio.Controller;
+
+
 /**
  * A very simple program using a graphical interface.
  * 
@@ -31,5 +50,86 @@ public final class SimpleGUIWithFileChooser {
      * update the UI: in this example the UI knows when should be updated, so
      * try to keep things separated.
      */
+    private final JFrame frame = new JFrame();
+    private static final int PROPORTION = 5;
+    private final Controller ctr;
 
+    /**
+     * builds a new {@link SimpleGUI}.
+     * @param ctr
+     */
+    public SimpleGUIWithFileChooser(final Controller ctr) {
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        this.ctr = ctr;
+        frame.setSize(sw / PROPORTION, sh / PROPORTION);
+        frame.setLocationByPlatform(true);
+
+    }
+
+    private void display() throws FileNotFoundException, IOException {
+        final JPanel canvas = new JPanel();
+        canvas.setLayout(new BorderLayout());
+        final JButton b1 = new JButton("Serialize");
+        final JTextArea t1 = new JTextArea();
+
+        final JPanel canvas2 = new JPanel(new BorderLayout());
+        final JButton buttonBrowse = new JButton("BROWSE");
+        final JTextField label = new JTextField();
+        final JFileChooser chooser  = new JFileChooser();
+
+        chooser.setFileFilter(new FileNameExtensionFilter("testo", "txt"));
+        canvas2.add(label, BorderLayout.CENTER);
+        canvas2.add(buttonBrowse, BorderLayout.LINE_END);
+        canvas.add(canvas2, BorderLayout.NORTH);
+        label.setEnabled(false);
+        label.setText(ctr.getPath());
+
+
+        buttonBrowse.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                if (chooser.showSaveDialog(canvas) == JFileChooser.APPROVE_OPTION) {
+                    try {
+                        ctr.setFile(chooser.getSelectedFile());
+                        label.setText(chooser.getSelectedFile().getPath());
+                    } catch (IOException e1) {
+                        JOptionPane.showMessageDialog(frame, e1, "Error", JOptionPane.ERROR_MESSAGE);
+                    }
+                }
+
+            }
+
+        });
+
+
+        frame.setContentPane(canvas);
+        canvas.add(t1, BorderLayout.CENTER);
+        canvas.add(b1, BorderLayout.SOUTH);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+
+
+        b1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                try {
+                    ctr.doSerial(t1.getText());
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+
+            }
+
+        });
+        frame.setVisible(true);
+    }
+
+    public static void main(final String... args) throws FileNotFoundException, IOException {
+        new SimpleGUIWithFileChooser(new Controller()).display();
+    }
 }
